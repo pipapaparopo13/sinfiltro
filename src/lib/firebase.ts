77a5746a304a9ff9b8837fdfb8398db6678ac799
pipapaparopo13 <imartinezmartinez1991@gmail.com>
2026-01-21 22:04:34 +0100

    Move Firebase config to environment variables to fix security alert

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, get, onValue, push, update, remove, runTransaction } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBE0S7-i4h8txM1q9VpEnRvSoxyLWdf9rw",
-    authDomain: "quiag-a6943.firebaseapp.com",
-    projectId: "quiag-a6943",
-    storageBucket: "quiag-a6943.firebasestorage.app",
-    messagingSenderId: "423254997376",
-    appId: "1:423254997376:web:6c0b73c5428d828b2b6965",
-    databaseURL: "https://quiag-a6943-default-rtdb.europe-west1.firebasedatabase.app"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL
 };
 
 const app = initializeApp(firebaseConfig);
